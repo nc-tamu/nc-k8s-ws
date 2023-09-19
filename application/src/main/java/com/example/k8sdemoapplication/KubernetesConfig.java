d6a845948b1b5497063121dd6499da9191e1ea55
Tarik Muhovic <tamu@netcompany.com>
2023-09-19 16:48:01 +0200

    Update deprecated class

--- application/src/main/java/com/example/k8sdemoapplication/KubernetesConfig.java
+++ application/src/main/java/com/example/k8sdemoapplication/KubernetesConfig.java
@@ -1,9 +1,7 @@
 package com.example.k8sdemoapplication;
 
-import io.fabric8.kubernetes.client.Config;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +10,7 @@ public class KubernetesConfig {
 
     @Bean
     public KubernetesClient kubernetesClient() {
-        Config config = new ConfigBuilder().build(); // You might need to customize this configuration.
-        return new DefaultKubernetesClient(config);
+        KubernetesClient client = new KubernetesClientBuilder().build(); // You might need to customize this configuration.
+        return client;
     }
 }
